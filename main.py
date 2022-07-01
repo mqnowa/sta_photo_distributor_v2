9e40be20ec6d488038110d4424f22aa7af92d03f
4vent <44436695+4vent@users.noreply.github.com>
2022-07-01 14:39:04 +0900

    完成

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import json
 
 import ui
 import photos
+import console
+import dialogs
+from objc_util import ObjCInstance
 
 def randcol():
     import random
@@ -26,7 +29,9 @@ VIEW_ALBUM = 'albums_scroll_view'
 VIEW_BUTTON_BG = 'controller_bg_view'
 BUTTON_NEXT = 'next_button'
 BUTTON_PREV = 'prev_button'
-BUTTON_SELECT = 'select_button'
+BUTTON_MENU = 'menu_button'
+
+COUNTER_VIEW = 'counter'
 
 with open('preferences.json') as f:
     CONFIG = json.load(f)
@@ -36,11 +41,23 @@ class image_view_controller():
         self.left  = left
         self.right = right
         self.view = view
-        self.assets = assets
+        self.__assets = assets
         
         # self.size = [view.height, view.width]
         
         self.view.content_mode = ui.CONTENT_SCALE_ASPECT_FIT if scale == 'fit' else ui.CONTENT_SCALE_ASPECT_FILL
+    
+    @property
+    def assets(self):
+        return self.__assets
+    
+    @assets.setter
+    def assets(self, assets):
+        self.__assets = assets
+        if self.left is not None:
+            self.left.assets = assets
+        if self.right is not None:
+            self.right.assets = assets
     
     def set_image(self, index, crop=False):
         # size = [int(self.view.width), int(self.view.height)]
@@ -63,6 +80,7 @@ class sta_photo_distributor_v2():
         
         self.__index = 0
         self.update_assets()
+        self.update_albums()
         self.MIVC = image_view_controller(
             self.v[VIEW_MSV][VIEW_MIV],
             self.assets,
@@ -73,13 +91,15 @@ class sta_photo_distributor_v2():
             self.v[VIEW_MSV].width,
             self.v[VIEW_MSV].height
         )
+        
+        self.__delete_mode = False
     
     def awake(self):
         # button function setting
         self.v[BUTTON_CLOSE].action = self.on_button_close
         self.v[VIEW_BUTTON_BG][BUTTON_NEXT].action = self.on_button_next
         self.v[VIEW_BUTTON_BG][BUTTON_PREV].action = self.on_button_prev
-        self.v[VIEW_BUTTON_BG][BUTTON_SELECT].action = self.on_button_select
+        self.v[VIEW_BUTTON_BG][BUTTON_MENU].action = self.on_button_menu
         
         # Thumbnail setting
         self.TB = thumbnails_view(self.v[VIEW_THUMB], 7)
@@ -91,6 +111,15 @@ class sta_photo_distributor_v2():
         self.v[VIEW_MSV].always_bounce_horizontal = True
         self.v[VIEW_MSV].always_bounce_vertical = True
         
+        self.AV = album_buttons_view(self.v[VIEW_ALBUM], self.on_button_select_album)
+        
+        # counter setting
+        ObjCInstance(self.v[COUNTER_VIEW]).clipsToBounds = True
+        self.v[COUNTER_VIEW].background_color = (1,1,1,0.6)
+        
+        # menu bar setting
+        self.v[VIEW_BUTTON_BG].background_color = (1,1,1,0.8)
+        
         self.open_last_image()
     
     def start(self):
@@ -98,6 +127,17 @@ class sta_photo_distributor_v2():
         
     def update_assets(self):
         self.assets = photos.get_assets()
+        try:
+            self.MIVC.assets = self.assets
+        except AttributeError:
+            pass
+        try:
+            self.TB.top_thumbnail.assets = self.assets
+        except AttributeError:
+            pass
+    
+    def update_albums(self):
+        self.albums = photos.get_albums()
     
     def open_image(self):
         self.MIVC.set_image(self.index)
@@ -105,6 +145,8 @@ class sta_photo_distributor_v2():
         with open(CONFIG['PATH_PREV_ID'], 'w') as f:
             f.write(id)
         self.TB.top_thumbnail.set_image(self.index, crop=True)
+        
+        self.v[COUNTER_VIEW].text = f'{self.index+1}/{len(self.assets)} ({len(self.assets)-self.index-1})'
     
     @property
     def index(self):
@@ -115,6 +157,15 @@ class sta_photo_distributor_v2():
             print('Index is out of assets length')
         self.__index = index
         self.open_image()
+    
+    @property
+    def delete_mode(self):
+        return self.__delete_mode
+    
+    @delete_mode.setter
+    def delete_mode(self, value):
+        self.__delete_mode = value
+        self.v[VIEW_ALBUM].background_color = (1, 0.3, 0.3) if self.__delete_mode else None
     
     def next_image(self):
         if self.index >= len(self.assets)-1:
@@ -147,10 +198,54 @@ class sta_photo_distributor_v2():
                 return
             except ValueError:
                 pass
+        if not self.open_from_prev_openings():
+            self.pick_image()
+    
+    def open_from_prev_openings(self):
+        if os.path.exists(CONFIG['PATH_PREV_IDs']):
+            with open(CONFIG['PATH_PREV_IDs']) as f:
+                ids = f.read().split()
+            for id in ids:
+                try:
+                    index = [asset.local_id for asset in self.assets].index(id)
+                    self.index = index
+                    return True
+                except ValueError:
+                    continue
+            
+            return False
+            
+    
+    def change_del_mode(self):
+        self.delete_mode = not self.delete_mode
+    
+    def add_to_album(self, sender):
+        album_name = sender.name
+        index = [a.title for a in self.albums].index(album_name)
+        self.albums[index].add_assets([self.assets[self.index]])
+        console.hud_alert(f'add to {album_name}', duration=0.5)
         
-        self.pick_image()
+        self.next_image()
+    
+    def del_from_album(self, sender):
+        album_name = sender.name
+        index = [a.title for a in self.albums].index(album_name)
+        self.albums[index].remove_assets([self.assets[self.index]])
+        console.hud_alert(f'remove from {album_name}', duration=0.5)
+    
+    def delete_current(self):
+        try:
+            photos.batch_delete([self.assets[self.index]])
+            self.update_assets()
+            self.open_image()
+        except IOError:
+            print('faild to del photo')
     
     def on_button_close(self, sender):
+        ids = [a.local_id for a in self.assets[:self.index+1]]
+        ids.reverse()
+        with open(CONFIG['PATH_PREV_IDs'], 'w') as f:
+            f.write('\n'.join(ids))
         self.v.superview.close()
 
     def on_button_next(self, sender):
@@ -159,8 +254,25 @@ class sta_photo_distributor_v2():
     def on_button_prev(self, sender):
         self.prev_image()
     
-    def on_button_select(self, sender):
-        self.pick_image()
+    def on_button_menu(self, sender):
+        option = console.alert('Menu', 
+            button1='画像を選択',
+            button2='アルバムから削除',
+            button3='端末から削除',
+            hide_cancel_button=True
+        )
+        if option == 1:
+            self.pick_image()
+        if option == 2:
+            self.change_del_mode()
+        if option == 3:
+            self.delete_current()
+    
+    def on_button_select_album(self, sender):
+        if self.delete_mode:
+            self.del_from_album(sender)
+        else:
+            self.add_to_album(sender)
 
 class thumbnails_view():
     def __init__(self, view, count):
@@ -214,6 +326,38 @@ class thumbnails_view():
         self.top_thumbnail = c
         self.set_views()
 
+class album_buttons_view():
+    def __init__(self, view, button_function):
+        self.view = view
+        
+        self.buttons = []
+        asset_collections = photos.get_albums()
+        for asset_collection in asset_collections:
+            name = asset_collection.title
+            button = ui.Button()
+            button.action = button_function
+            button.bounds = (0, 0, 48, 48)
+            button.corner_radius = 24
+            button.background_color = (0.9,0.9,0.9,0.8)
+            button.name = name
+            if name.startswith('$'):
+                button.title = name[1]
+            else:
+                button.title = name[0]
+            self.buttons.append(button)
+        
+        width = len(self.buttons) * (self.buttons[0].width + 8) + 8
+        height = self.view.content_size[1]
+        self.view.content_size = (width, height)
+        
+        x = 8
+        for button in self.buttons:
+            button.center = (0, self.view.height/2)
+            button.x = x
+            self.view.add_subview(button)
+            
+            x += button.width + 8
+
 def remove_splash():
     def animation():
         sv['splash'].alpha = 0.0
@@ -237,7 +381,11 @@ v.height -= CONFIG['STATUS_BAR_HEIGHT']
 v.y      += CONFIG['STATUS_BAR_HEIGHT']
 sv.add_subview(splash)
 
-sv.present('fullscreen', hide_title_bar=True)
+sv.present(
+    'fullscreen',
+    hide_title_bar=True,
+    orientations=['portrait', 'portrait-upside-down']
+)
 
 SPD2 = sta_photo_distributor_v2(v)
 SPD2.awake()
